858fe203bff3b8da5298a118247ada0b5e707ed4
fisayo-dev <olufisayobadina@gmail.com>
2024-12-09 13:11:59 +0100

    creating vote card and main page

--- src/components/VoteCard.jsx
+++ src/components/VoteCard.jsx
@@ -1,7 +1,14 @@
 const VoteCard = () => {
   return (
-    <div>VoteCard</div>
-  )
-}
+    <div className="bg-zinc-700 hover:bg-zinc-600 rounded-md">
+      <img src="" alt="" />
+      <div className="py-3 px-4">
+        <h2 className="text-[1.1rem]">
+          Who will be the next ballon dor winner ?
+        </h2>
+      </div>
+    </div>
+  );
+};
 
-export default VoteCard
+export default VoteCard;
--- src/components/VoteLists.jsx
+++ src/components/VoteLists.jsx
@@ -1,7 +1,22 @@
+import VoteCard from "./VoteCard";
+
 const VoteLists = () => {
   return (
-    <div>VoteLists</div>
-  )
-}
+    <div className="mt-10 text-white">
+      <div className="container mx-auto md:px-20 px-5 ">
+        <div className="grid gap-5 grid-cols-5">
+          <VoteCard />
+          <VoteCard />
+          <VoteCard />
+          <VoteCard />
+          <VoteCard />
+          <VoteCard />
+          <VoteCard />
+          <VoteCard />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default VoteLists
+export default VoteLists;
