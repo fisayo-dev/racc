9f3fb9c9db1f03e5b40604b3926e8e202d5d2e99
fisayo-dev <olufisayobadina@gmail.com>
2024-12-09 19:03:26 +0100

    degsigning signup form

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,15 +1,15 @@
 import { Button } from "@/components/ui/button";
-
+import { Forward } from "iconsax-react";
 const Signup = () => {
   return (
     <div className="text-zinc-200 grid gap-3">
-      <h2 className="md:text-4xl text-3xl text-blue-300 font-bold text-center py-10 ">
+      <h2 className="md:text-4xl text-3xl text-blue-300 font-bold text-center py-5 ">
         Racc
       </h2>
       <div className="grid gap-5">
         <div className="text-center">1....2....3....4</div>
-        <div className="my-5 md:w-5/12 sm:w-3/5 w-4/5 shadow-md mx-auto bg-zinc-700 rounded-lg p-5">
-          <div className="grid gap-5">
+        <div className=" md:w-5/12 sm:w-3/5 w-full shadow-md mx-auto bg-zinc-700 md:rounded-xl rounded-3xl p-5 my-5">
+          <div className="grid gap-6">
             <h2 className="text-left text-3xl font-bold">Hi, Lets get to know you</h2>
             <div className="grid gap-3">
               <div className="grid md:flex items-center md:gap-10 gap-3">
@@ -54,8 +54,11 @@ const Signup = () => {
                   placeholder="Re-enter your password"
                 />
               </div>
-              <Button className="!px-3 !py-3">Next</Button>
             </div>
+              <Button type="icon">
+                <p>Next</p>
+                <Forward />
+              </Button>
           </div>
         </div>
       </div>
