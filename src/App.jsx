e122194c7d7b606772de94a6864bae5020561dc7
fisayo-dev <olufisayobadina@gmail.com>
2024-12-09 11:13:38 +0100

    routed all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import { Home, Signup, Login } from "./pages";
 function App() {
-  return (
-    <>
-      <div className="text-2xl font-bold">Hello World</div>
-    </>
-  )
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/home" element={<Home />} /> */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </>
+    )
+  );
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
