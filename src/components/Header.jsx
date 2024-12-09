55300b4490f3c4d412664c44b988c323c9530e78
fisayo-dev <olufisayobadina@gmail.com>
2024-12-09 17:46:49 +0100

    completed formating

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,14 +5,14 @@ const Header = () => {
     <div className="bg-zinc-900 shadow-md fixed bottom-100 top-0 w-full py-5 border-b-[0.1rem] border-zinc-700 text-white">
       <div className="container mx-auto md:px-20 px-5 flex gap-20 justify-between items-center">
         <h2 className="text-2xl text-blue-300 font-bold">Racc </h2>
-              <div className="hidden md:flex items-center gap-4 bg-zinc-600 w-3/5 rounded-lg px-4 py-3">
-                  <Filter className="h-6 w-6"/>
+        <div className="hidden md:flex items-center gap-4 bg-zinc-600 w-3/5 rounded-lg px-4 py-3">
+          <Filter className="h-6 w-6" />
           <input
             type="text"
             className="search-input w-full text-slate-100"
             placeholder="Search for votes by name or tags"
-                  />
-                  <SearchNormal1 className="h-6 w-6"/>
+          />
+          <SearchNormal1 className="h-6 w-6" />
         </div>
         <Button className="border-[0.1rem] text-[1rem] text-blue-300 hover:bg-zinc-700 border-zinc-700 rounded-lg">
           Sign in to vote
