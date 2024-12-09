f7eb6ea10effdb20828fbe9847b4ba090f9d1097
fisayo-dev <olufisayobadina@gmail.com>
2024-12-09 12:18:53 +0100

    fixed esm and require module conflict

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,8 @@
-import { Button } from "@/components/ui/button";
+import Header from "../components/Header";
+
 const Home = () => {
   return (
-    <div className="bg-zinc-900 py-3 text-white">
-      <div className="container mx-auto md:px-20 px-5 flex justify-between items-center">
-        <h2 className="text-2xl font-bold">Racc</h2>
-        <Button className="border-2 hover:bg-zinc-700 border-zinc-700">Sign in to vote</Button>
-      </div>
-    </div>
+    <Header />
   );
 };
 
