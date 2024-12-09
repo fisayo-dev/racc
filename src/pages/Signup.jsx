e45e27a2eeef55765c783a778dee1e9236f4c548
fisayo-dev <olufisayobadina@gmail.com>
2024-12-09 19:23:34 +0100

    integrating select shadcn component

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,4 +1,11 @@
 import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { Forward } from "iconsax-react";
 const Signup = () => {
   return (
@@ -15,14 +22,16 @@ const Signup = () => {
         </div>
         <div className=" md:w-5/12 sm:w-3/5 w-full shadow-md mx-auto bg-zinc-700 md:rounded-xl rounded-t-3xl p-5 my-5">
           <div className="grid gap-6">
-            <h2 className="text-center text-3xl font-bold">Hi, Lets get to know you</h2>
+            <h2 className="text-center text-3xl font-bold">
+              Hi, Lets get to know you
+            </h2>
             <div className="grid gap-3">
               <div className="grid md:flex items-center md:gap-10 gap-3">
                 <div className="grid gap-2 w-full shadow-md">
                   <label className="font-bold">First Name:</label>
                   <input
                     type="text"
-                    className="border-[0.1rem] text-[0.9rem] text-[0.9rem] border-zinc-400 py-3 px-3 rounded-md w-full"
+                    className="border-[0.1rem] text-[0.9rem] border-zinc-400 py-3 px-3 rounded-md w-full"
                     placeholder="Ex. John "
                   />
                 </div>
@@ -30,7 +39,7 @@ const Signup = () => {
                   <label className="font-bold">Last Name:</label>
                   <input
                     type="text"
-                    className="border-[0.1rem] text-[0.9rem] text-[0.9rem] border-zinc-400 py-3 px-3 rounded-md w-full"
+                    className="border-[0.1rem]  text-[0.9rem] border-zinc-400 py-3 px-3 rounded-md w-full"
                     placeholder="Ex. Doe "
                   />
                 </div>
@@ -60,33 +69,43 @@ const Signup = () => {
                 />
               </div>
             </div>
-              <Button type="icon">
-                <p>Next</p>
-                <Forward />
-              </Button>
+            <Button type="icon">
+              <p>Next</p>
+              <Forward />
+            </Button>
           </div>
         </div>
         <div className=" md:w-5/12 sm:w-3/5 w-full shadow-md mx-auto bg-zinc-700 md:rounded-xl rounded-t-3xl p-5 my-5">
           <div className="grid gap-6">
-            <h2 className="text-center text-3xl font-bold">Tell us more about you, Fisayo</h2>
+            <h2 className="text-center text-3xl font-bold">
+              Tell us more about yourself, Fisayo
+            </h2>
             <div className="grid gap-3">
-              <div className="grid md:flex items-center md:gap-10 gap-3">
-                <div className="grid gap-2 w-full shadow-md">
-                  <label className="font-bold">First Name:</label>
-                  <input
-                    type="text"
-                    className="border-[0.1rem] text-[0.9rem] border-zinc-400 py-3 px-3 rounded-md w-full"
-                    placeholder="Ex. John "
-                  />
-                </div>
-                <div className="grid gap-2 w-full shadow-md">
-                  <label className="font-bold">Last Name:</label>
-                  <input
-                    type="text"
-                    className="border-[0.1rem] text-[0.9rem] border-zinc-400 py-3 px-3 rounded-md w-full"
-                    placeholder="Ex. Doe "
-                  />
-                </div>
+              <div className="grid gap-2 w-full shadow-md">
+                <label className="font-bold">Natinality:</label>
+                <Select className="outline-none">
+                  <SelectTrigger >
+                    <SelectValue placeholder="Select your country" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="light">Nigeria</SelectItem>
+                    <SelectItem value="dark">South America</SelectItem>
+                    <SelectItem value="system">China</SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+              <div className="grid gap-2 w-full shadow-md">
+                <label className="font-bold">Last Name:</label>
+                <Select className="outline-none">
+                  <SelectTrigger >
+                    <SelectValue placeholder="Select your country" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="light">Nigeria</SelectItem>
+                    <SelectItem value="dark">South America</SelectItem>
+                    <SelectItem value="system">China</SelectItem>
+                  </SelectContent>
+                </Select>
               </div>
               <div className="grid gap-2">
                 <label className="font-bold">Email address:</label>
@@ -113,10 +132,10 @@ const Signup = () => {
                 />
               </div>
             </div>
-              <Button type="icon">
-                <p>Next</p>
-                <Forward />
-              </Button>
+            <Button type="icon">
+              <p>Next</p>
+              <Forward />
+            </Button>
           </div>
         </div>
       </div>
