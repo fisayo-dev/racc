f667e6dcee0dc3e0a14a7a4d9c325913ab30c03d
fisayo-dev <olufisayobadina@gmail.com>
2024-12-09 17:47:34 +0100

    change search bar bg color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <div className="bg-zinc-900 shadow-md fixed bottom-100 top-0 w-full py-5 border-b-[0.1rem] border-zinc-700 text-white">
       <div className="container mx-auto md:px-20 px-5 flex gap-20 justify-between items-center">
         <h2 className="text-2xl text-blue-300 font-bold">Racc </h2>
-        <div className="hidden md:flex items-center gap-4 bg-zinc-600 w-3/5 rounded-lg px-4 py-3">
+        <div className="hidden md:flex items-center gap-4 bg-zinc-700 w-3/5 rounded-lg px-4 py-3">
           <Filter className="h-6 w-6" />
           <input
             type="text"
