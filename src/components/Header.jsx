8e9aba026cac2e4e17c13a866d7d8abe3d0e8490
fisayo-dev <olufisayobadina@gmail.com>
2024-12-09 12:57:46 +0100

    working on the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,12 @@ const Header = () => {
     <div className="bg-zinc-900 py-3 text-white">
       <div className="container mx-auto md:px-20 px-5 flex justify-between items-center">
         <h2 className="text-2xl font-bold">Racc</h2>
-        <Button className="border-2 text-[1rem] hover:bg-zinc-700 border-zinc-700">Sign in to vote</Button>
+        <Button className="border-2 text-[1rem] hover:bg-zinc-700 border-zinc-700">
+          Sign in to vote
+        </Button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
