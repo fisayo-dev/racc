fd5661fb210edb225c489e748d99ce921c3cf469
fisayo-dev <olufisayobadina@gmail.com>
2024-12-09 13:34:49 +0100

    creating dummy voting lists

--- src/components/VoteCard.jsx
+++ src/components/VoteCard.jsx
@@ -1,8 +1,8 @@
-const VoteCard = () => {
+const VoteCard = ({image,options,title,status,voters,start_date}) => {
   return (
     <div className="bg-zinc-700 hover:bg-zinc-600 rounded-md">
-      <img src="" alt="" />
-      <div className="py-3 px-4">
+      <img src={image} alt="" />
+          <div className="py-3 px-4">
         <h2 className="text-[1.1rem]">
           Who will be the next ballon dor winner ?
         </h2>
