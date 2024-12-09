57af527d11a346b71e233ca749ea0fa755424cbc
fisayo-dev <olufisayobadina@gmail.com>
2024-12-09 12:00:17 +0100

    creating header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import { Button } from "@/components/ui/button";
 const Home = () => {
   return (
-    <div className="bg-zinc-900">
-      <Button>Click me</Button>
+    <div className="bg-zinc-900 py-3 text-white">
+      <div className="container mx-auto md:px-20 px-5 flex justify-between items-center">
+        <h2 className="text-2xl font-bold">Racc</h2>
+        <Button className="border-2 hover:bg-zinc-700 border-zinc-700">Sign in to vote</Button>
+      </div>
     </div>
   );
 };
