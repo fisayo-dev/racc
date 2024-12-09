402b0e8b672de1cfff915bc921b5d5e87e6a30b3
fisayo-dev <olufisayobadina@gmail.com>
2024-12-09 16:25:45 +0100

    changes border radius from md to lg

--- src/components/VoteCard.jsx
+++ src/components/VoteCard.jsx
@@ -60,24 +60,24 @@ const VoteCard = ({
     }
   };
   return (
-    <div className="bg-zinc-800 text-zinc-100 border-[0.1rem] border-zinc-800 hover:border-zinc-500 cursor-pointer shadow-md rounded-md grid gap-2">
+    <div className="bg-zinc-800 text-zinc-100 border-[0.1rem] border-zinc-800 hover:border-zinc-500 cursor-pointer shadow-md rounded-lg grid gap-2">
       <div className="grid items-center py-3 px-4 gap-4">
         <h2 className="md:text-[1.1rem] text-[1.4rem] text-zinc-200 font-bold">
           {description}
         </h2>
         <div className="flex items-center gap-2 flex-wrap">
           {tags.map((tag) => (
-            <div className="px-3 text-[0.75rem] border border-zinc-700 hover:text-gray-300 text-gray-400 py-2 rounded-md">
+            <div className="px-3 text-[0.75rem] border border-zinc-700 hover:text-gray-300 text-gray-400 py-2 rounded-lg">
               #{tag}
             </div>
           ))}
         </div>
-        <div className="flex rounded-md shadow-md overflow-clip items-stretch">
+        <div className="flex rounded-lg shadow-md overflow-clip items-stretch">
           <img src={image} className="w-full" alt="" />
         </div>
         {/* <div className="grid gap-[0.4rem]">
           {options.map((opt) => (
-            <div className="px-4 py-3 hover:shadow-md border-[0.1rem] hover:bg-zinc-700 border-zinc-700 rounded-md text-[1.0.5rem]">
+            <div className="px-4 py-3 hover:shadow-md border-[0.1rem] hover:bg-zinc-700 border-zinc-700 rounded-lg text-[1.0.5rem]">
               {opt}
             </div>
           ))}
