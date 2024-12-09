edceea93a009e579b9e95c4dc5970ab7d7490add
fisayo-dev <olufisayobadina@gmail.com>
2024-12-09 19:18:18 +0100

    changed general font of input

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -22,7 +22,7 @@ const Signup = () => {
                   <label className="font-bold">First Name:</label>
                   <input
                     type="text"
-                    className="border-[0.1rem] border-zinc-400 py-3 px-3 rounded-md w-full"
+                    className="border-[0.1rem] text-[0.9rem] text-[0.9rem] border-zinc-400 py-3 px-3 rounded-md w-full"
                     placeholder="Ex. John "
                   />
                 </div>
@@ -30,7 +30,7 @@ const Signup = () => {
                   <label className="font-bold">Last Name:</label>
                   <input
                     type="text"
-                    className="border-[0.1rem] border-zinc-400 py-3 px-3 rounded-md w-full"
+                    className="border-[0.1rem] text-[0.9rem] text-[0.9rem] border-zinc-400 py-3 px-3 rounded-md w-full"
                     placeholder="Ex. Doe "
                   />
                 </div>
@@ -39,7 +39,7 @@ const Signup = () => {
                 <label className="font-bold">Email address:</label>
                 <input
                   type="text"
-                  className="border-[0.1rem] border-zinc-400 py-3 px-3 rounded-md w-full"
+                  className="border-[0.1rem] text-[0.9rem] border-zinc-400 py-3 px-3 rounded-md w-full"
                   placeholder="Ex. johndoe@gmail.com"
                 />
               </div>
@@ -47,7 +47,7 @@ const Signup = () => {
                 <label className="font-bold">Password:</label>
                 <input
                   type="password"
-                  className="border-[0.1rem] border-zinc-400 py-3 px-3 rounded-md w-full"
+                  className="border-[0.1rem] text-[0.9rem] border-zinc-400 py-3 px-3 rounded-md w-full"
                   placeholder="Must not be less than 6 digits"
                 />
               </div>
@@ -55,7 +55,60 @@ const Signup = () => {
                 <label className="font-bold">Repeat password:</label>
                 <input
                   type="password"
-                  className="border-[0.1rem] border-zinc-400 py-3 px-3 rounded-md w-full"
+                  className="border-[0.1rem] text-[0.9rem] border-zinc-400 py-3 px-3 rounded-md w-full"
+                  placeholder="Re-enter your password"
+                />
+              </div>
+            </div>
+              <Button type="icon">
+                <p>Next</p>
+                <Forward />
+              </Button>
+          </div>
+        </div>
+        <div className=" md:w-5/12 sm:w-3/5 w-full shadow-md mx-auto bg-zinc-700 md:rounded-xl rounded-t-3xl p-5 my-5">
+          <div className="grid gap-6">
+            <h2 className="text-center text-3xl font-bold">Tell us more about you, Fisayo</h2>
+            <div className="grid gap-3">
+              <div className="grid md:flex items-center md:gap-10 gap-3">
+                <div className="grid gap-2 w-full shadow-md">
+                  <label className="font-bold">First Name:</label>
+                  <input
+                    type="text"
+                    className="border-[0.1rem] text-[0.9rem] border-zinc-400 py-3 px-3 rounded-md w-full"
+                    placeholder="Ex. John "
+                  />
+                </div>
+                <div className="grid gap-2 w-full shadow-md">
+                  <label className="font-bold">Last Name:</label>
+                  <input
+                    type="text"
+                    className="border-[0.1rem] text-[0.9rem] border-zinc-400 py-3 px-3 rounded-md w-full"
+                    placeholder="Ex. Doe "
+                  />
+                </div>
+              </div>
+              <div className="grid gap-2">
+                <label className="font-bold">Email address:</label>
+                <input
+                  type="text"
+                  className="border-[0.1rem] text-[0.9rem] border-zinc-400 py-3 px-3 rounded-md w-full"
+                  placeholder="Ex. johndoe@gmail.com"
+                />
+              </div>
+              <div className="grid gap-2">
+                <label className="font-bold">Password:</label>
+                <input
+                  type="password"
+                  className="border-[0.1rem] text-[0.9rem] border-zinc-400 py-3 px-3 rounded-md w-full"
+                  placeholder="Must not be less than 6 digits"
+                />
+              </div>
+              <div className="grid gap-2">
+                <label className="font-bold">Repeat password:</label>
+                <input
+                  type="password"
+                  className="border-[0.1rem] text-[0.9rem] border-zinc-400 py-3 px-3 rounded-md w-full"
                   placeholder="Re-enter your password"
                 />
               </div>
