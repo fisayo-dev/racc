b95580934befadd8577b7605bbaa3a6c57c67e27
fisayo-dev <olufisayobadina@gmail.com>
2024-12-09 15:57:03 +0100

    increased blur intensity

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 const Header = () => {
   return (
-    <div className="bg-transparent shadow-md backdrop-blur-sm fixed bottom-100 top-0 w-full py-5 border-b-[0.1rem] border-zinc-700 text-white">
+    <div className="bg-transparent shadow-md backdrop-blur fixed bottom-100 top-0 w-full py-5 border-b-[0.1rem] border-zinc-700 text-white">
       <div className="container mx-auto md:px-20 px-5 flex justify-between items-center">
         <h2 className="text-2xl text-blue-300 font-bold">Racc </h2>
         <Button className="border-[0.1rem] text-[1rem] text-blue-300 hover:bg-zinc-700 border-zinc-700">
