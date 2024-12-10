ee8e41c4ed4edc9dc8e892b280a7883ff9f91cb5
fisayo-dev <olufisayobadina@gmail.com>
2024-12-10 14:45:01 +0100

    vote desc : linked icon to home page

--- src/pages/vote/VoteDescription.jsx
+++ src/pages/vote/VoteDescription.jsx
@@ -1,15 +1,18 @@
 import { ArrowLeft } from "iconsax-react";
 import { Header } from "../../components";
+import { Link } from "react-router-dom";
 
 const VoteDescription = () => {
   return (
     <div className="grid gap-2 text-zinc-200">
       <Header />
-      <div className="container mx-auto md:px-20 px-5 my-[8rem]">
+          <div className="container mx-auto md:px-20 px-5 my-[8rem]">
+              <Link to="/">
         <button className="my-3 rounded-lg text-zinc-800 flex items-center gap-1 hover:bg-zinc-200 bg-zinc-300 px-4 py-3">
           <ArrowLeft className="h-6 w-6" />
           <p>Back</p>
-        </button>
+                  </button>
+                  </Link>
       </div>
     </div>
   );
