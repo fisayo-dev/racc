3ce78a622f8d6bf0f3071fdb1ce4ffb52d7004e6
fisayo-dev <olufisayobadina@gmail.com>
2024-12-10 14:57:03 +0100

    creating vote description page

--- src/pages/vote/VoteDescription.jsx
+++ src/pages/vote/VoteDescription.jsx
@@ -1,18 +1,49 @@
-import { ArrowLeft } from "iconsax-react";
+import { ArrowLeft, Check } from "iconsax-react";
 import { Header } from "../../components";
 import { Link } from "react-router-dom";
+import { CheckBadgeIcon } from "@heroicons/react/24/outline";
 
 const VoteDescription = () => {
   return (
     <div className="grid gap-2 text-zinc-200">
       <Header />
-          <div className="container mx-auto md:px-20 px-5 my-[8rem]">
-              <Link to="/">
-        <button className="my-3 rounded-lg text-zinc-800 flex items-center gap-1 hover:bg-zinc-200 bg-zinc-300 px-4 py-3">
-          <ArrowLeft className="h-6 w-6" />
-          <p>Back</p>
-                  </button>
-                  </Link>
+      <div className="container mx-auto md:px-20 px-5 my-[8rem]">
+        <div className="grid gap-5">
+          <Link to="/">
+            <button className="my-1 rounded-lg text-zinc-800 flex items-center gap-1 hover:bg-zinc-200 bg-zinc-300 px-4 py-3">
+              <ArrowLeft className="h-6 w-6" />
+              <p>Back</p>
+            </button>
+          </Link>
+          <div className="grid gap-10 items-start md:grid-cols-2">
+            <div className="grid gap-5">
+              <h2 className="md:text-3xl text-2xl">
+                Who is the greatest football player of all time?
+              </h2>
+              <img src="/vite.svg" className="w-1/6" alt="" />
+            </div>
+            <div className="grid gap-5">
+              <div className="grid gap-2">
+                <h2 className="md:text-3xl text-2xl">Options:</h2>
+                <p className="text-md">Pick an option to vote</p>
+              </div>
+              <div className="grid gap-3">
+                <div className="px-4 py-3 rounded-lg hover:bg-zinc-800  border-[0.102rem] border-zinc-200 cursor-pointer">
+                    <div className="flex items-center justify-between">
+                  <p>Lionel Messi</p>
+                  <CheckBadgeIcon className="text-green-400 h-6 w-6"/>
+                    </div>
+                </div>
+                <div className="px-4 py-3 rounded-lg hover:bg-zinc-800  border-[0.102rem] border-zinc-200 cursor-pointer">
+                    <div className="flex items-center justify-between">
+                  <p>Christiano Ronaldo</p>
+                  <CheckBadgeIcon className="text-green-400 h-6 w-6"/>
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
