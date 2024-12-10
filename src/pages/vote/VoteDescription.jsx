c55f6ad1eac9d6f15f09a496e1581c9432214fd9
fisayo-dev <olufisayobadina@gmail.com>
2024-12-10 15:27:37 +0100

    updating vote descrition content to show properties of votes

--- src/pages/vote/VoteDescription.jsx
+++ src/pages/vote/VoteDescription.jsx
@@ -1,11 +1,18 @@
 import { ArrowLeft, Check, Clock } from "iconsax-react";
 import image4 from "../../assets/samples/nextjs.png";
 import { Header } from "../../components";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { CheckBadgeIcon } from "@heroicons/react/24/outline";
 import { PiGraph } from "react-icons/pi";
+import { fakeVotingList } from "../../components/VoteLists";
+import { Calendar } from "lucide-react";
 
 const VoteDescription = () => {
+  const { id } = useParams();
+
+  const particularVote = fakeVotingList.filter((vote) => vote.id == id);
+  const { description, voters, status, tags, start_date, end_date, options } =
+    particularVote[0];
   return (
     <div className="grid gap-2 text-zinc-200">
       <Header />
@@ -19,27 +26,48 @@ const VoteDescription = () => {
           </Link>
           <div className="grid gap-10 items-start md:grid-cols-2">
             <div className="grid gap-8">
-              <h2 className="md:text-3xl text-2xl">
-                Who is the greatest football player of all time?
-              </h2>
+              <h2 className="md:text-3xl text-2xl">{description}</h2>
               <div className="shadow-md rounded-lg overflow-hidden">
                 <img src={image4} className="w-full" alt="" />
               </div>
               <div className="grid gap-3">
-                <div className="flex text-green-400 items-center gap-2">
-                  <Clock className="w-6 h-6" />
-                  <p>Ongoing - 2 days left</p>
+                <div
+                  className={`capitalize flex ${
+                    status == "upcoming" && "text-blue-400"
+                  } ${status == "ended" && "text-red-400"} ${
+                    status == "ongoing" && "text-green-400"
+                  } items-center gap-2`}
+                >
+                  {status == "upcoming" && <Calendar className="w-6 h-6" />}
+                  {status == "ended" && <Check className="w-6 h-6" />}
+                  {status == "ongoing" && <Clock className="w-6 h-6" />}
+                  <p>{status}</p>
                 </div>
-                <div className="flex text-yellow-400 items-center gap-2">
+                <div
+                  className={`flex ${
+                    status == "ended" && "text-red-400"
+                  } ${
+                    status == "ongoing" && "text-green-400"
+                  } items-center gap-2`}
+                >
                   <PiGraph className="w-6 h-6" />
-                  <p>11 people voting</p>
+                  {voters.length > 0 ? (
+                    <p>
+                      {voters.length} people{" "}
+                      {status == "ended" ? "voted" : "are voting"}
+                    </p>
+                  ) : (
+                    <p>No voters available</p>
+                  )}
                 </div>
               </div>
             </div>
             <div className="grid gap-5">
               <div className="grid gap-2">
                 <h2 className="md:text-3xl text-2xl">Options:</h2>
-                <p className="text-md">You have particpated in this voting exercise</p>
+                <p className="text-md">
+                  You have particpated in this voting exercise
+                </p>
               </div>
               <div className="grid gap-3">
                 <div className="px-4 py-3 rounded-lg hover:bg-zinc-800  border-[0.102rem] border-zinc-200 cursor-pointer">
