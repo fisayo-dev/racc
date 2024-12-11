702abfc69e6f09a8b3c0139a23cfe40d59f606d3
fisayo-dev <olufisayobadina@gmail.com>
2024-12-11 23:37:35 +0100

    created publicity forms

--- src/pages/User/CreateVote.jsx
+++ src/pages/User/CreateVote.jsx
@@ -1,18 +1,26 @@
 import { Hashtag, Paperclip } from "iconsax-react";
 import { Header } from "../../components";
 import { ListBulletIcon } from "@heroicons/react/24/outline";
+import { MegaphoneIcon } from "lucide-react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 const CreateVote = () => {
   return (
     <div className="w-full grid text-zinc-200">
       <Header />
       <div className="mt-[8rem]">
         <div className="container grid mx-auto xl:px-20 md:px-15 px-5 ">
-          <div className="grid sm:gap-[7.5rem] md:gap-[10rem] grid-cols-1 md:grid-cols-2">
+          <div className="grid gap[2.5rem] sm:gap-[7.5rem] md:gap-[10rem] items-start grid-cols-1 md:grid-cols-2">
             <div className="grid gap-[5rem] my-5">
               <div className="grid gap-4">
                 <div className="flex items-center gap-2">
-                  <Paperclip className="h-6 w-6" />
-                  <h2 className="text-3xl font-bold">Vote Info:</h2>
+                  <Paperclip className="h-7 w-7" />
+                  <h2 className="text-3xl font-bold">Vote Info</h2>
                 </div>
                 <div className="grid gap-4">
                   <label>Vote Title</label>
@@ -46,7 +54,7 @@ const CreateVote = () => {
               </div>
               <div className="grid gap-4">
                 <div className="flex items-center gap-2">
-                  <ListBulletIcon className="h-6 w-6" />
+                  <ListBulletIcon className="h-7 w-7" />
                   <h2 className="text-3xl font-bold">Vote Options:</h2>
                 </div>
                 <div className="grid gap-4">
@@ -69,20 +77,53 @@ const CreateVote = () => {
               </div>
             </div>
             <div className="grid gap-[5rem] my-5">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
-                nam consequatur minus voluptatum atque nostrum unde aspernatur
-                beatae cum. Distinctio ab eaque praesentium, debitis dolor hic
-                quia dicta odit alias, repudiandae esse eius, numquam molestiae
-                nobis vitae? Aut quisquam distinctio quos facere provident,
-                excepturi nulla iste laborum. Incidunt eum libero architecto
-                nam, dolor maiores rerum error hic molestias ratione aut. Autem
-                quas quis eum corrupti nobis enim distinctio, vitae minima,
-                delectus amet deleniti sint repellat ad. Ipsam delectus esse
-                labore debitis sit aut impedit repellendus quod, quae soluta
-                porro sint velit eaque inventore rem ex! Expedita at explicabo
-                quibusdam. Consectetur.
-              </p>
+              <div className="grid gap-4">
+                <div className="flex items-center gap-2">
+                  <MegaphoneIcon className="h-7 w-7" />
+                  <h2 className="text-3xl font-bold">Publicity & Audience</h2>
+                </div>
+                <div className="grid gap-4">
+                  <h2 className="text-md">
+                    Do you want any gender restriction ?
+                  </h2>
+                  <Select className="outline-none">
+                    <SelectTrigger>
+                      <SelectValue placeholder="Choose an option" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="dark">Yes</SelectItem>
+                      <SelectItem value="system">No</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
+                <div className="grid gap-4">
+                  <h2 className="text-md">
+                    Which gender so you want to restrict?
+                  </h2>
+                  <Select className="outline-none">
+                    <SelectTrigger>
+                      <SelectValue placeholder="Pick a gender" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="male">Male</SelectItem>
+                      <SelectItem value="female">Female</SelectItem>
+                      <SelectItem value="others">Others</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
+                <div className="grid gap-4">
+                  <h2 className="text-md">Do you want the vote to be public</h2>
+                  <Select className="outline-none">
+                    <SelectTrigger>
+                      <SelectValue placeholder="Choose an option" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="female">Yes</SelectItem>
+                      <SelectItem value="male">No</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
+              </div>
             </div>
           </div>
         </div>
