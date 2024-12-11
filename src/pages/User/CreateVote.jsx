b8d059774dc2e926c4d762d45a9116a3d4c5879d
fisayo-dev <olufisayobadina@gmail.com>
2024-12-11 23:02:36 +0100

    creating create-vote page form layout

--- src/pages/User/CreateVote.jsx
+++ src/pages/User/CreateVote.jsx
@@ -1,10 +1,30 @@
-import { Header } from "../../components"
+import { Paperclip } from "iconsax-react";
+import { Header } from "../../components";
 const CreateVote = () => {
   return (
-    <>
-    <Header />
-  </>
-  )
-}
+    <div className="text-zinc-200">
+      <Header />
+      <div className="container mx-auto xl:px-20 md:px-15 px-5 ">
+              <div className="mt-[8rem]">
+                  <div className="grid gap-5 md:gap-8 grid-cols-1 md:grid-cols-2">
+                      <div className="grid gap-5">
+                          <div className="grid gap-4">
+                              <div className="flex items-center gap-2">
+                                  <Paperclip className="h-6 w-6" />
+                                  <h2 className="text-3xl font-bold">Vote Info:</h2>
+                              </div>
+                              <div className="grid gap-3">
+                                  <label>Vote Title</label>
+                                  <input type="text"  className="border-[0.1rem] border-zinc-300 rounded-lg px-4 py-3" placeholder="Enter a title that best describes your vote"/>
+                              </div>
+                          </div>
+                      </div>
+                      <div className="grid gap-5"></div>
+                  </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default CreateVote
+export default CreateVote;
