d2319f97ec0c007c7895a7d15167383d4666dc03
fisayo-dev <olufisayobadina@gmail.com>
2024-12-12 22:09:03 +0100

    imported header to profile page

--- src/pages/User/Notifications.jsx
+++ src/pages/User/Notifications.jsx
@@ -1,11 +1,16 @@
-import { Header } from "../../components"
+import { Header } from "../../components";
 
 const Notifications = () => {
   return (
     <div>
       <Header />
+      <div className="mt-[8rem] mb-[5rem]">
+        <div className="container grid mx-auto xl:px-20 md:px-15 px-5 ">
+          Notifcations
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Notifications
+export default Notifications;
