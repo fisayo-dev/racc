42217f4b2a453005e10b524adaa98c0c4d165d38
fisayo-dev <olufisayobadina@gmail.com>
2024-12-13 13:32:35 +0100

    created the first card of user profile data

--- src/pages/User/Profile.jsx
+++ src/pages/User/Profile.jsx
@@ -1,6 +1,7 @@
 import { Button, Header } from "../../components";
 import { useAuth } from "../../context/AuthContext";
-import image from '../../assets/samples/Nextjs.png'
+import image from "../../assets/samples/Nextjs.png";
+import { BarChart, BarChart2Icon, Barcode } from "lucide-react";
 
 const Profile = () => {
   const { user, logoutUser } = useAuth();
@@ -10,17 +11,38 @@ const Profile = () => {
       <Header />
       <div className="mt-[8rem] mb-[5rem]">
         <div className="app-container ">
-          <div className="profile-grid">
-            <div className="border-[0.1rem] border-zinc-500 bg-zinc-800 rounded-lg px-5 py-4">
+          <div className="grid md:flex profile-grid">
+            <div className="bg-zinc-800 rounded-lg p-6">
               <div className="grid gap-4">
-                <div className="h-52 w-52 mx-auto bg-cover bg-center overflow-hidden rounded-full bg-zinc-300" style={{ backgroundImage: `url(${image})` }}>
-                </div>
+                <div
+                  className="h-40 w-40 mx-auto bg-cover bg-center overflow-hidden rounded-full bg-zinc-300"
+                  style={{ backgroundImage: `url(${image})` }}
+                ></div>
                 <div className="text-center">
                   <p>{user.name}</p>
                   <p className="text-sm">{user.email}</p>
                 </div>
                 <div className="mx-auto" onClick={logoutUser}>
-                  <Button text="Logout" className="bg-red-500 border-none text-white hover:bg-red-600"/>
+                  <Button
+                    text="Logout"
+                    className="bg-red-500 border-none text-white hover:bg-red-600"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="w-full bg-zinc-800 rounded-lg p-6">
+              <div className="grid gap-2">
+                <h2 className="text-2xl font-bold">Your Activity</h2>
+                <div className="grid gap-5 my-5 grid-cols-1 md:grid-cols-3">
+                  <div className="grid place-items-center items-center bg-zinc-700 h-64 shadow rounded-lg p-6">
+                    <h2 className="text-xl font-bold">Total Votes</h2>
+                    <div className="grid text-center justify-center">
+                      <p className="text-8xl font-bold">24</p>
+                      <p className="text-sm">votes</p>
+                    </div>
+                  </div>
+                  <div className="bg-zinc-700 h-64 shadow rounded-lg p-6"></div>
+                  <div className="bg-zinc-700 h-64 shadow rounded-lg p-6"></div>
                 </div>
               </div>
             </div>
