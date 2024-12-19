9d29899085d64962f392ab2fc51d44f14a5cba9e
fisayo-dev <olufisayobadina@gmail.com>
2024-12-19 19:56:54 +0100

    script: placed loading display message

--- src/pages/User/UserVoteEdit.jsx
+++ src/pages/User/UserVoteEdit.jsx
@@ -171,179 +171,192 @@ const UserVoteEdit = () => {
   return (
     <div>
       <Header />
-      <div className="app-container my-[8rem] grid gap-10">
-        <h2 className="text-center font-bold text-4xl">Edit Vote</h2>
-        <div className="grid gap-3 mx-auto 2xl:w-3/5 md:w-4/6">
-          <div className="grid gap-4">
-            <label>Vote Title</label>
-            <input
-              type="text"
-              className="border-[0.1rem] border-zinc-300 rounded-lg px-4 py-3"
-              placeholder="Enter a title that best describes your vote"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-            />
-          </div>
-          <div className="grid gap-4">
-            <label>Vote Description</label>
-            <textarea
-              type="text"
-              className="border-[0.1rem] border-zinc-300 rounded-lg px-4 py-3"
-              placeholder="Tell us what your vote is all about"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-            ></textarea>
-          </div>
-          <div className="grid gap-4">
-            <label>Vote Image</label>
-            <p className="text-sm">
-              Upload an image about your vote to increase visiblilty.{" "}
-            </p>
-            <div
-              type="text"
-              className="grid justify-items-center place-content-center shadow-sm rounded-lg h-[400px] bg-cover bg-center  bg-zinc-800"
-              placeholder="Tell us what your vote is all about"
-              style={{ backgroundImage: `url(${backgroundImage})` }}
-            >
-              {!backgroundImage && (
-                <div className="grid gap-1 place-items-center">
-                  <ImageIcon className="h-[8rem] w-[8rem]" />
-                  <p>No image uploaded</p>
+      <div className="app-container my-[8rem]">
+        {loading && (
+          <Loader2Icon className="mx-auto h-24 w-24 md:h-28 md:w-28 animate-spin my-10"/>
+        )}
+        {!loading && (
+          <div className="grid gap-10">
+            <h2 className="text-center font-bold text-4xl">Edit Vote</h2>
+            <div className="grid gap-3 mx-auto 2xl:w-3/5 md:w-4/6">
+              <div className="grid gap-4">
+                <label>Vote Title</label>
+                <input
+                  type="text"
+                  className="border-[0.1rem] border-zinc-300 rounded-lg px-4 py-3"
+                  placeholder="Enter a title that best describes your vote"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+              </div>
+              <div className="grid gap-4">
+                <label>Vote Description</label>
+                <textarea
+                  type="text"
+                  className="border-[0.1rem] border-zinc-300 rounded-lg px-4 py-3"
+                  placeholder="Tell us what your vote is all about"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                ></textarea>
+              </div>
+              <div className="grid gap-4">
+                <label>Vote Image</label>
+                <p className="text-sm">
+                  Upload an image about your vote to increase visiblilty.{" "}
+                </p>
+                <div
+                  type="text"
+                  className="grid justify-items-center place-content-center shadow-sm rounded-lg h-[400px] bg-cover bg-center  bg-zinc-800"
+                  placeholder="Tell us what your vote is all about"
+                  style={{ backgroundImage: `url(${backgroundImage})` }}
+                >
+                  {!backgroundImage && (
+                    <div className="grid gap-1 place-items-center">
+                      <ImageIcon className="h-[8rem] w-[8rem]" />
+                      <p>No image uploaded</p>
+                    </div>
+                  )}
                 </div>
-              )}
-            </div>
-            <div
-              className={`relative overflow-hidden justify-center hover:bg-zinc-700 bg-zinc-800 flex px-4 py-3 rounded-lg cursor-pointer items-center gap-2 shadow-lg `}
-            >
-              <input
-                type="file"
-                accept="image/*"
-                id="image-upload"
-                onChange={handleImageUpload}
-                className="absolute opacity-0 h-full w-full cursor-pointer"
-              ></input>
-              <ImageUp className="h-6 w-6 cursor-pointer" />
-              <p>{backgroundImage ? "Change the image" : "Upload an image"}</p>
-            </div>
-          </div>
-          <div className="grid gap-4">
-            <label>Vote Tags</label>
-            <div className="grid lg:flex gap-3">
-              <div className="w-full flex items-center gap-2 border-[0.1rem] border-zinc-300 rounded-lg px-4 py-3">
-                <Hashtag className="h-6 w-6" />
-                <input
-                  type="text"
-                  className="w-full "
-                  placeholder="Tag 1"
-                  value={tag1}
-                  onChange={(e) => setTag1(e.target.value)}
-                />
-              </div>
-              <div className="w-full flex items-center gap-2 border-[0.1rem] border-zinc-300 rounded-lg px-4 py-3">
-                <Hashtag className="h-6 w-6" />
-                <input
-                  type="text"
-                  className="w-full "
-                  placeholder="Tag 2"
-                  value={tag2}
-                  onChange={(e) => setTag2(e.target.value)}
-                />
-              </div>
-            </div>
-          </div>
-          <div className="grid gap-4">
-            <h2 className="text-md">
-              What date do you want the vote to commence ?
-            </h2>
-            <Popover>
-              <PopoverTrigger asChild>
-                <Button
-                  variant={"outline"}
-                  className={cn(
-                    "bg-zinc-700 hover:bg-zinc-600 text-gray-200 justify-start text-left font-normal",
-                    !date && "text-gray-300"
-                  )}
+                <div
+                  className={`relative overflow-hidden justify-center hover:bg-zinc-700 bg-zinc-800 flex px-4 py-3 rounded-lg cursor-pointer items-center gap-2 shadow-lg `}
                 >
-                  <CalendarIcon className="mr-2 h-4 w-4" />
-                  {date ? format(date, "PPP") : <span>Pick a date</span>}
-                </Button>
-              </PopoverTrigger>
-              <PopoverContent className="w-auto p-0">
-                <Calendar
-                  mode="single"
-                  selected={date}
-                  onSelect={setDate}
-                  initialFocus
-                />
-              </PopoverContent>
-            </Popover>
-          </div>
-          <div className="grid gap-4">
-            <h2 className="text-md">What date do you want the vote to end?</h2>
-            <Popover>
-              <PopoverTrigger asChild>
-                <Button
-                  variant={"outline"}
-                  className={cn(
-                    "bg-zinc-700 hover:bg-zinc-600 text-gray-200 justify-start text-left font-normal",
-                    !date2 && "text-gray-300"
-                  )}
-                >
-                  <CalendarIcon className="mr-2 h-4 w-4" />
-                  {date2 ? format(date2, "PPP") : <span>Pick a date</span>}
-                </Button>
-              </PopoverTrigger>
-              <PopoverContent className="w-auto p-0">
-                <Calendar
-                  mode="single"
-                  selected={date2}
-                  onSelect={setDate2}
-                  initialFocus
-                />
-              </PopoverContent>
-            </Popover>
-          </div>
-          <div className="grid gap-4">
-            <h2 className="text-md ">Vote Options:</h2>
-            {options.map((option, index) => (
-              <div key={index} className="grid gap-4">
-                <label>Option {index + 1}</label>
-                <div className="flex gap-2">
                   <input
-                    type="text"
-                    className="border-[0.1rem] border-zinc-300 rounded-lg px-4 py-3 flex-1"
-                    placeholder="Enter option title"
-                    value={option.title}
-                    onChange={(e) => updateOptionTitle(index, e.target.value)}
-                  />
-                  {options.length > 2 && (
-                    <button
-                      className="hover:text-red-400 text-red-500"
-                      onClick={() => removeOption(index)}
-                    >
-                      <Trash />
-                    </button>
-                  )}
+                    type="file"
+                    accept="image/*"
+                    id="image-upload"
+                    onChange={handleImageUpload}
+                    className="absolute opacity-0 h-full w-full cursor-pointer"
+                  ></input>
+                  <ImageUp className="h-6 w-6 cursor-pointer" />
+                  <p>
+                    {backgroundImage ? "Change the image" : "Upload an image"}
+                  </p>
                 </div>
               </div>
-            ))}
-            <button
-              onClick={addOption}
-              className="ml-auto mt-2 bg-zinc-600 text-white px-4 py-2 rounded-lg"
+              <div className="grid gap-4">
+                <label>Vote Tags</label>
+                <div className="grid lg:flex gap-3">
+                  <div className="w-full flex items-center gap-2 border-[0.1rem] border-zinc-300 rounded-lg px-4 py-3">
+                    <Hashtag className="h-6 w-6" />
+                    <input
+                      type="text"
+                      className="w-full "
+                      placeholder="Tag 1"
+                      value={tag1}
+                      onChange={(e) => setTag1(e.target.value)}
+                    />
+                  </div>
+                  <div className="w-full flex items-center gap-2 border-[0.1rem] border-zinc-300 rounded-lg px-4 py-3">
+                    <Hashtag className="h-6 w-6" />
+                    <input
+                      type="text"
+                      className="w-full "
+                      placeholder="Tag 2"
+                      value={tag2}
+                      onChange={(e) => setTag2(e.target.value)}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="grid gap-4">
+                <h2 className="text-md">
+                  What date do you want the vote to commence ?
+                </h2>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        "bg-zinc-700 hover:bg-zinc-600 text-gray-200 justify-start text-left font-normal",
+                        !date && "text-gray-300"
+                      )}
+                    >
+                      <CalendarIcon className="mr-2 h-4 w-4" />
+                      {date ? format(date, "PPP") : <span>Pick a date</span>}
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0">
+                    <Calendar
+                      mode="single"
+                      selected={date}
+                      onSelect={setDate}
+                      initialFocus
+                    />
+                  </PopoverContent>
+                </Popover>
+              </div>
+              <div className="grid gap-4">
+                <h2 className="text-md">
+                  What date do you want the vote to end?
+                </h2>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        "bg-zinc-700 hover:bg-zinc-600 text-gray-200 justify-start text-left font-normal",
+                        !date2 && "text-gray-300"
+                      )}
+                    >
+                      <CalendarIcon className="mr-2 h-4 w-4" />
+                      {date2 ? format(date2, "PPP") : <span>Pick a date</span>}
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0">
+                    <Calendar
+                      mode="single"
+                      selected={date2}
+                      onSelect={setDate2}
+                      initialFocus
+                    />
+                  </PopoverContent>
+                </Popover>
+              </div>
+              <div className="grid gap-4">
+                <h2 className="text-md ">Vote Options:</h2>
+                {options.map((option, index) => (
+                  <div key={index} className="grid gap-4">
+                    <label>Option {index + 1}</label>
+                    <div className="flex gap-2">
+                      <input
+                        type="text"
+                        className="border-[0.1rem] border-zinc-300 rounded-lg px-4 py-3 flex-1"
+                        placeholder="Enter option title"
+                        value={option.title}
+                        onChange={(e) =>
+                          updateOptionTitle(index, e.target.value)
+                        }
+                      />
+                      {options.length > 2 && (
+                        <button
+                          className="hover:text-red-400 text-red-500"
+                          onClick={() => removeOption(index)}
+                        >
+                          <Trash />
+                        </button>
+                      )}
+                    </div>
+                  </div>
+                ))}
+                <button
+                  onClick={addOption}
+                  className="ml-auto mt-2 bg-zinc-600 text-white px-4 py-2 rounded-lg"
+                >
+                  Add Option
+                </button>
+                <div className="text-md flex items-center justify-end hover:bg-zinc-300 bg-zinc-500"></div>
+              </div>
+            </div>
+
+            <div
+              onClick={editVote}
+              className="flex px-4 py-3 rounded-lg item-center overflow-hidden mx-auto bg-zinc-300 text-zinc-900 cursor-pointer hover:bg-zinc-200 items-center gap-2 justify-center"
             >
-              Add Option
-            </button>
-            <div className="text-md flex items-center justify-end hover:bg-zinc-300 bg-zinc-500"></div>
+              <Edit2 className="h-6 w-6" />
+              <p>Edit vote</p>
+            </div>
           </div>
-        </div>
-        {loading && <Loader2Icon className="animate-spin h-32 w-32 mx-auto" />}
-        <div
-          onClick={editVote}
-          className="flex px-4 py-3 rounded-lg item-center overflow-hidden mx-auto bg-zinc-300 text-zinc-900 cursor-pointer hover:bg-zinc-200 items-center gap-2 justify-center"
-        >
-          <Edit2 className="h-6 w-6" />
-          <p>Edit vote</p>
-        </div>
+        )}
       </div>
     </div>
   );
