c52daa469f06e728d30e4d26ae0d06230d55eeff
fisayo-dev <olufisayobadina@gmail.com>
2024-12-20 19:56:41 +0100

    script: notification delete func

--- src/pages/User/Notifications.jsx
+++ src/pages/User/Notifications.jsx
@@ -2,15 +2,39 @@ import { useEffect, useState } from "react";
 import { Header } from "../../components";
 import db from "../../appwrite/databases";
 import { Query } from "appwrite";
-import { Loader2Icon } from "lucide-react";
-import { InfoCircle } from "iconsax-react";
+import { ExternalLinkIcon, Loader2Icon } from "lucide-react";
+import { InfoCircle, Trash } from "iconsax-react";
 import { useAuth } from "../../context/AuthContext";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const Notifications = () => {
-  const { user, notificationSeen, setNotificationSeen } = useAuth();
-
+  const { user, setNotificationSeen } = useAuth();
   const [notifications, setNotifications] = useState(null);
+
+  const deleteNotification = async (notification_id) => {
+    try {
+      // Delete notification from database
+      await db.notifications.delete(notification_id);
+      // Update the state by filtering out the deleted notification
+      setNotifications((notifications) =>
+        notifications.filter((notification) => notification.$id !== notification_id)
+      );
+
+      Swal.fire({
+        toast: true,
+        position: "bottom-end",
+        icon: "success",
+        title: `Notification successfully deleted`,
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+      });
+    } catch (err) {
+      console.log("Error:", err.message);
+    }
+  };
+
   const getTheUserFullName = async (userId) => {
     try {
       const results = await db.users.list([Query.equal("user_id", userId)]);
@@ -100,31 +124,53 @@ const Notifications = () => {
             {notifications && notifications.length !== 0 && (
               <div className="grid gap-4">
                 {notifications.map((notification) => (
-                  <Link
-                    to={`/vote/${notification.vote_id}`}
+                  <div
                     key={notification.$id}
-                    className="bg-zinc-700 rounded-xl flex items-center px-5 hover:bg-zinc-600 cursor-pointer hover:shadow-md h-[4rem]"
+                    className="bg-zinc-800 rounded-xl flex items-center px-5 hover:bg-zinc-700 cursor-pointer hover:shadow-md h-[4rem]"
                   >
-                    <div className="flex items-center   gap-5">
-                      <div
-                        className="h-8 w-8 md:h-11 md:w-11 rounded-full bg-zinc-400 bg-cover bg-center"
-                        style={{
-                          backgroundImage: `url(${`https://cloud.appwrite.io/v1/storage/buckets/${
-                            import.meta.env.VITE_PROFILE_IMAGES_BUCKET_ID
-                          }/files/${notification.fromUserImage}/view?project=${
-                            import.meta.env.VITE_PROJECT_ID
-                          }`})`,
-                        }}
-                      />
-                      {notification.from == user.$id ? (
-                        <h2>You participated in your vote</h2>
-                      ) : (
-                        <h2>
-                          {notification.fromFullName} participated in your vote
-                        </h2>
-                      )}
+                    <div className="w-full flex items-center justify-between   gap-5">
+                      <div className="flex items-center gap-4">
+                        <div
+                          className="h-8 w-8 md:h-11 md:w-11 rounded-full bg-zinc-400 bg-cover bg-center"
+                          style={{
+                            backgroundImage: `url(${`https://cloud.appwrite.io/v1/storage/buckets/${
+                              import.meta.env.VITE_PROFILE_IMAGES_BUCKET_ID
+                            }/files/${
+                              notification.fromUserImage
+                            }/view?project=${
+                              import.meta.env.VITE_PROJECT_ID
+                            }`})`,
+                          }}
+                        />
+                        <div className="">
+                          {notification.from == user.$id ? (
+                            <h2 className="">You participated in your vote</h2>
+                          ) : (
+                            <h2>
+                              {notification.fromFullName} participated in your
+                              vote
+                            </h2>
+                          )}
+                        </div>
+                      </div>
+
+                      <div className="flex items-center gap-2">
+                        <Link
+                          to={`/vote/${notification.vote_id}`}
+                          className="grid place-items-center justify-center"
+                          onClick={() => deleteNotification(notification.$id)}
+                        >
+                          <ExternalLinkIcon className="h-6 w-6 text-blue-600 hover:text-blue-500" />
+                        </Link>
+                        <div
+                          className="grid place-items-center justify-center"
+                          onClick={() => deleteNotification(notification.$id)}
+                        >
+                          <Trash className="h-6 w-6 text-red-600 hover:text-red-500" />
+                        </div>
+                      </div>
                     </div>
-                  </Link>
+                  </div>
                 ))}
               </div>
             )}
