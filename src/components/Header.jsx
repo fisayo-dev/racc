2cb39322d6872810df0e81eec779f368c586fcb2
fisayo-dev <olufisayobadina@gmail.com>
2024-12-20 17:46:38 +0100

    script: gave az-idx to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -35,7 +35,7 @@ const Header = () => {
   };
 
   return (
-    <div className="bg-zinc-900 shadow-md fixed bottom-100 top-0 w-full py-5 border-b-[0.1rem] border-zinc-700 text-white">
+    <div className="bg-zinc-900 shadow-md fixed bottom-100 top-0 w-full py-5 border-b-[0.1rem] border-zinc-700 text-white z-40">
       <div className="app-container flex gap-20 justify-between items-center">
         <Link to="/" className="text-2xl text-blue-300 font-bold">
           Racc
